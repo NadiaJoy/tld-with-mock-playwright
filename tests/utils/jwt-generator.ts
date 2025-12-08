f1e43a90f766b70f19bb230d5ef412268fd2b8c9
vladimirovodenko <vladimir@tallinn-learning.ee>
2025-12-08 18:58:42 +0200

    formatting

--- tests/utils/jwt-generator.ts
+++ tests/utils/jwt-generator.ts
@@ -1,8 +1,8 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 
 export function fakeJwt() {
-  const secret = 'fake-secret';
-  const payload = { userId: '123' };
+  const secret = 'fake-secret'
+  const payload = { userId: '123' }
 
-  return jwt.sign(payload, secret);
+  return jwt.sign(payload, secret)
 }
